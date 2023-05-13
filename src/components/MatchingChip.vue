db574d371c314d3d9aeb83e56edc70d4fe581f76
cc41516 <b05201028@ntu.edu.tw>
2023-05-13 12:54:11 +0800

    add matching question UI

---
+++ src/components/MatchingChip.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-chip outline square size="lg" color="black" class="cursor-pointer">
+    {{ label }}
+  </q-chip>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string;
+}
+
+defineProps<Props>();
+</script>
--- src/components/MatchingTable.vue
+++ src/components/MatchingTable.vue
@@ -2,62 +2,71 @@
   <div>
     <q-markup-table separator="cell" flat bordered class="q-mb-lg">
       <thead>
-        <tr>
-          <th class="text-center"><div class="text-h5">圖片</div></th>
-          <th class="text-center"><div class="text-h5">名稱</div></th>
-          <th class="text-center"><div class="text-h5">適應症</div></th>
+        <tr class="row">
+          <th class="text-center col-3"><div class="text-h6">圖片</div></th>
+          <th class="text-center col-3"><div class="text-h6">名稱</div></th>
+          <th class="text-center col-6"><div class="text-h6">適應症</div></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(reply, index) in currReply" :key="index">
-          <td class="text-center">{{ images[index] }}</td>
+        <tr v-for="(reply, index) in currReply" :key="index" class="row">
+          <td class="col-3 text-center">
+            <img :src="images[index]" class="full-width" />
+          </td>
           <draggable
-            tag="td"
             :list="reply.name"
-            group="name"
-            item-key=""
+            :group="{
+              name: 'name',
+              put: checkPutName,
+            }"
+            tag="td"
+            :component-data="{ className: 'col-3 scroll hide-scrollbar' }"
             @change="updateReply"
+            v-bind="dragContainerOptions"
           >
             <template #item="{ element }">
-              <q-chip size="lg"> {{ element }} </q-chip>
+              <MatchingChip :label="element" />
             </template>
           </draggable>
           <draggable
-            tag="td"
             :list="reply.indication"
             group="indication"
-            item-key=""
+            tag="td"
+            :component-data="{
+              className: 'col-6 column items-start scroll hide-scrollbar',
+            }"
             @change="updateReply"
+            v-bind="dragContainerOptions"
           >
             <template #item="{ element }">
-              <q-chip size="lg"> {{ element }} </q-chip>
+              <MatchingChip :label="element" />
             </template>
           </draggable>
         </tr>
       </tbody>
     </q-markup-table>
 
-    <div class="row items-center">
+    <div class="row items-center rounded-borders">
       <div class="col-1 text-h6">名稱</div>
       <draggable
         :list="remainingNames"
         group="name"
-        item-key=""
+        v-bind="dragContainerOptions"
         class="col-11 q-my-lg"
       >
         <template #item="{ element }">
-          <q-chip size="lg"> {{ element }} </q-chip>
+          <MatchingChip :label="element" />
         </template>
       </draggable>
       <div class="col-1 text-h6">適應症</div>
       <draggable
         :list="remainingIndications"
         group="indication"
-        item-key=""
+        v-bind="dragContainerOptions"
         class="col-11 q-my-lg"
       >
         <template #item="{ element }">
-          <q-chip size="lg"> {{ element }} </q-chip>
+          <MatchingChip :label="element" />
         </template>
       </draggable>
     </div>
@@ -65,9 +74,11 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import draggable from "vuedraggable";
 import { MatchingCase, MatchingCaseReply } from "@/store/matching";
+import MatchingChip from "./MatchingChip.vue";
+import { shuffle, getImageUrl } from "@/utils/common.ts";
 
 interface Props {
   cases: MatchingCase[];
@@ -79,21 +90,41 @@ const emit = defineEmits<{
   (e: "select", reply: MatchingCaseReply[]): void;
 }>();
 
-// manipulate this variable to render the page, and when it changes, emit an event to update MatchingStore
+// manipulate currReply to render the page, and when it changes, emit an event to update MatchingStore
 const currReply: MatchingCaseReply[] = reactive(props.reply);
 
 const _allNames: string[] = getItems(props.cases, "name");
 const _allIndications: string[] = getItems(props.cases, "indication");
+shuffle(_allNames);
+shuffle(_allIndications);
 const _allReplyNames: string[] = getItems(currReply, "name");
 const _allReplyIndications: string[] = getItems(currReply, "indication");
 
-const images: string[] = props.cases.map((c) => c.image);
+const images: string[] = props.cases.map((c) => getImageUrl(c.image));
 const remainingNames: string[] = reactive(
   _allNames.filter((item) => !_allReplyNames.includes(item))
 );
 const remainingIndications: string[] = reactive(
   _allIndications.filter((item) => !_allReplyIndications.includes(item))
 );
+
+// draggable configurations
+const dragContainerOptions = {
+  itemKey: "",
+  ghostClass: "ghost",
+  chosenClass: "chosen",
+  animation: 200,
+  onStart: () => (drag.value = true),
+  onEnd: () => (drag.value = false),
+};
+
+const drag = ref(false);
+
+function checkPutName(to: any, from: any) {
+  const fromName = from.options.group.name;
+  const toEmpty = !to.el.children.length;
+  return fromName && toEmpty;
+}
 
 function getItems(
   arr: MatchingCase[] | MatchingCaseReply[],
@@ -109,3 +140,17 @@ function updateReply() {
   emit("select", currReply);
 }
 </script>
+
+<style scoped lang="scss">
+.ghost {
+  opacity: 0.5;
+}
+
+.chosen {
+  border: 2px solid $secondary;
+}
+
+.no-move {
+  transition: transform 0s;
+}
+</style>
