98aea039831bbc336817ee6836b240e339b506f0
cc41516 <b05201028@ntu.edu.tw>
2023-06-09 02:11:39 +0800

    add delete operation in backstage

--- src/pages/AdminPage.vue
+++ src/pages/AdminPage.vue
@@ -5,20 +5,38 @@
       後台管理系統
     </div>
     <div v-if="!isAdmin" class="row">
-      <q-input outlined v-model="password" label="請輸入密碼" type="password" />
-      <q-btn label="確認" @click="confirm" />
+      <q-input outlined v-model="password" label="請輸入密碼" type="password" @keyup.enter="confirm" />
+      <q-btn label="確認" @click="confirm" class="q-mx-md" />
     </div>
     <div v-if="isAdmin">
       <q-table :rows="users" :columns="columns" row-key="name" flat>
         <template v-slot:body-cell-name="props">
-        <q-td :props="props">
-          <q-btn unelevated :label="props.value" no-caps @click="intoUser(props.row.id)" size="md" />
-        </q-td>
+          <q-td :props="props">
+            <q-btn flat :label="props.value" no-caps @click="intoUser(props.row.id)" size="md" color="secondary" />
+          </q-td>
+        </template>
+        <template v-slot:body-cell-delete="props">
+          <q-td :props="props">
+            <q-btn flat label="刪除紀錄" @click="onDeleting(props.row.id)" size="md" color="negative" />
+          </q-td>
       </template>
+
       </q-table>
     </div>
-    <q-btn label="清除使用者" @click="myDeleteUser" />
-    <q-btn label="清除所有使用者" @click="myDeleteAllUsers" />
+    <q-btn label="清除所有使用者" @click="myDeleteAllUsers" color="negative" />
+
+    <q-dialog v-model="deleteUserPrompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div>注意：一旦刪除資料，便無法再復原。</div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="取消" color="secondary" v-close-popup />
+          <q-btn flat label="確定" color="negative" v-close-popup @click="myDeleteUser(toBeDeletedId)" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -31,7 +49,9 @@ import {
   deleteAllUsers,
 } from "../server/controller.ts";
 import { QTableColumn } from 'quasar'
-import { usePreChoiceStore } from "@/store/choice";
+import { usePreChoiceStore, usePostChoiceStore } from "@/store/choice";
+import { useVideoStore } from "@/store/video";
+import { usePreMatchingStore, usePostMatchingStore } from "@/store/matching";
 import { hash } from "@/utils/common.ts"
 import { User } from "@/server/controller.ts"
 
@@ -39,9 +59,11 @@ const router = useRouter();
 
 const password = ref('')
 const isAdmin = ref(false)
+const deleteUserPrompt = ref(false)
+const toBeDeletedId = ref('')
 
 let users: User[] = reactive([])
-const bodyStyle: string = 'font-size: 12px' 
+const bodyStyle: string = 'font-size: 14px' 
 const headerStyle: string = 'font-size: 16px' 
 let columns: QTableColumn[] = [
   {name: 'name', label: '姓名', align: 'center', field: 'name'},
@@ -52,6 +74,7 @@ let columns: QTableColumn[] = [
   {name: 'videoScore', label: '影片題組', field: 'videoScore', sortable: true},
   {name: 'preMatchingScore', label: '後測（選擇題）', field: 'preMatchingScore', sortable: true},
   {name: 'postMatchingScore', label: '後測（配合題）', field: 'postMatchingScore', sortable: true},
+  {name: 'delete', label: '', field: 'name'}
 ]
 columns = columns.map(c => {
   return {...c, style: bodyStyle, headerStyle: headerStyle}
@@ -77,6 +100,7 @@ async function confirm() {
 async function getData() {
   try {
     const res = await getAllUsers() ?? []
+    users.length = res.length
     res.forEach((val, index) => {
       users[index] = val
     })
@@ -90,21 +114,35 @@ function intoUser(id: string) {
   router.push({name: 'home'})
 }
 
+function onDeleting(id: string) {
+  toBeDeletedId.value = id
+  deleteUserPrompt.value = true
+}
 
-async function myDeleteUser() {
-  let id = localStorage.getItem("id");
-  if (id !== null) {
-    deleteUser(id);
-    usePreChoiceStore().reset();
-    localStorage.clear();
+async function myDeleteUser(id: string) {
+  await deleteUser(id);
+  await getData()
+  if (id === localStorage.getItem('id')) {
+    clear()
   }
+  toBeDeletedId.value = ''
 }
 
 async function myDeleteAllUsers() {
   await deleteAllUsers();
-  usePreChoiceStore().reset();
-  localStorage.clear()
+  await getData()
+  clear()
 }
+
+function clear() {
+  localStorage.clear();
+  usePreChoiceStore().reset();
+  usePostChoiceStore().reset();
+  useVideoStore().reset();
+  usePreMatchingStore().reset();
+  usePostMatchingStore().reset();
+}
+
 
 </script>
 
